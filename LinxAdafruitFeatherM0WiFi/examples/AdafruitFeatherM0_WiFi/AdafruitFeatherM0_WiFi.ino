6bd4530a373d1ee4b9632ede554b4e3e9d976f8a
cbcGirard <cgirard@usc.edu>
2020-12-14 15:08:44 -0800

    updated documentation

--- LinxAdafruitFeatherM0WiFi/examples/AdafruitFeatherM0_WiFi/AdafruitFeatherM0_WiFi.ino
+++ LinxAdafruitFeatherM0WiFi/examples/AdafruitFeatherM0_WiFi/AdafruitFeatherM0_WiFi.ino
@@ -11,6 +11,7 @@
 
 // Leave uncommented if network name+password programmed in sketch
   #define WIFI_HARDCODED
+// WARNING: firmware is currently not functional if the above is commented out!!! After programming the Feather, the USB connection will no longer work. To rescue the Feather, double-tap its reset button to enter a recovery mode, where the red LED will pulse and a new USB device will appear to the PC. Select this new port and upload a working firmware to restore the Feather.
 
 //Include All Peripheral Libraries Used By LINX
 #include <SPI.h>
@@ -45,14 +46,15 @@ void setup()
   //Start Wifi Listener.  Settings (IP, SSID, etc) Will Be Read From Non Volatile Storage And Can Be Set Using LINX VIs Via USB
   #ifndef WIFI_HARDCODED
    LinxWifiConnection.Start(LinxDevice);
-#else
+  #else
   //Or, set manually: 
 
- LinxWifiConnection.SetSsid(THE_SSID);
- LinxWifiConnection.SetSecurity(WPA2_PASSPHRASE);  //NONE, WPA2_PASSPHRASE, WPA2_KEY, WEP40, WEO104
- LinxWifiConnection.SetPassphrase(THE_PW);    			   
- LinxWifiConnection.Start(LinxDevice, 0, 0, 0, 0, 44300);  //Start With Fixed IP and Port.  When Using This Method You Cannot Update The IP/Port Using LINX VIs
-#endif
+  LinxWifiConnection.SetSsid(THE_SSID);
+  LinxWifiConnection.SetSecurity(WPA2_PASSPHRASE);  //NONE, WPA2_PASSPHRASE, WPA2_KEY, WEP40, WEO104
+  LinxWifiConnection.SetPassphrase(THE_PW);    			   
+  LinxWifiConnection.Start(LinxDevice, 0, 0, 0, 0, 44300);  //Start With Fixed IP and Port.  When Using This Method You Cannot Update The IP/Port Using LINX VIs
+ // Use an IP address of 0.0.0.0 for DHCP; router will assign address
+  #endif
 
 }
 
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # LINX-firmware-for-Feather-M0
 
-Copy both LinxAdafruitFeatherM0WiFi and LinxSerialListener folders into the Arduino libraries directory.
+After downloading this repo, copy its three folders into the Arduino libraries directory (i.e., LinxAdafruitFeatherM0WiFi and the others must be directly under Arduino/libraries).
 
-To load firmware onto the microcontroller, open the sketch under Examples>LINX>LinxAdafruitFeatherM0WiFi>AdafruitFeatherM0_WiFi_Serial and upload to Feather M0 board
+The Arduino IDE must be configured to use the Adafruit Feather board; follow the instructions at https://learn.adafruit.com/adafruit-feather-m0-wifi-atwinc1500/setup and the following pages (Using with Arduino IDE and Using the WiFi Module). In addition to the WiFi101 library, you will also need to install the FlashStorage library to use this firmware.
+
+To load firmware onto the microcontroller, open the sketch under Examples>LINX>LinxAdafruitFeatherM0WiFi>AdafruitFeatherM0_Serial and upload to Feather M0 board
+
+To communicate with Labview over WiFi, instead upload the AdafruitFeatherM0_WiFi sketch after changing the SSID (network name) and password in wifiInfo.h to match your home network.
+
+NB: re-configuring WiFi from Labview is currently not functional, and loading that firmware version will disable the board's USB connection. If that happens, double-press the board's reset button to enter recovery mode, and upload a working firmware (e.g. the Blink example) to the newly-created port.
